51305929835784260590e124c8dc88178a5bf728
gustavo-barrios2006 <gabarrios2006@gmail.com>
2025-02-08 19:16:36 -0300

    Implementando sistema de atualização correta das estatísticas quando a composição do robô muda.

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+const pecas = {
+    "braços": {
+        "força": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "força": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "núcleos":{
+        "força": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "força": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "força": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 document.querySelectorAll(".controle-ajuste").forEach(element=>{
-    element.addEventListener("click", event=>{
+    element.addEventListener("click", event=>{        
         manipulaDados(event.target.dataset.controle=='+'?"somar":"subtrair", event.target.parentNode.querySelector("[data-contador]"));
+        atualizaEstatistica(event.target.dataset.peca, event.target.dataset.controle=='+'?"somar":"subtrair");
     });
 });
 function manipulaDados(operacao, campo)
@@ -13,4 +47,16 @@ function manipulaDados(operacao, campo)
     {
         campo.value=parseInt(campo.value)+1;
     }
+}
+function atualizaEstatistica(peca, operacao)
+{ 
+Array.from(document.querySelectorAll("p[data-estatistica]")).forEach(element=>{
+    if(operacao=="somar")
+    {
+element.textContent=parseInt(element.textContent)+pecas[peca][element.dataset.estatistica];
+    }
+    else{
+        element.textContent=parseInt(element.textContent)-pecas[peca][element.dataset.estatistica];
+    }
+});
 }
